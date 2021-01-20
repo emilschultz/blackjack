e761415504efcf42ed7be575fc0f0cb93a6fcee7
Emil Schultz <emilrschultz@gmail.com>
2021-01-20 14:51:51 +0100

    draw a card function

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const [cardDeck, setCardDeck] = useState('');
   // const [dealerHand, setDealerHand] = useState('');
   // const [playerHand, setPlayerHand] = useState('');
   const [responseData, setResponseData] = useState('');
-
-  const handeClick = (event) => {
+    
+  // SHUFFLE THE CARDS
+  const handleShuffleCards = (event) => {
     event.preventDefault();
-
+    
     let shuffleCards = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1' 
       fetch(shuffleCards)
         .then(response => {
@@ -21,13 +21,36 @@ function App() {
           console.log(data)
           setResponseData(data)
         })
-  }
+      }
+
+    // DRAW A CARD
+    const handleDrawCards = (event) => {
+      event.preventDefault();
+
+      let drawCards = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2'
+          fetch(drawCards)
+            .then(response => {
+              console.log(response.status)
+              console.log(response.text)
+              return response.json();
+            })
+            .then(data => {
+              console.log(data)
+              setResponseData(data)
+            })
+    }
+
+
+  
+
+
 
 
   return (
     <>
       <h1>Let's play blackjack!</h1>
-      <button type="submit" onClick={event => handeClick(event)}>Hit me</button>
+      <button type="submit" onClick={event => handleShuffleCards(event)}>Shuffle the cards / restart?</button>
+      <button type="submit" onClick={event => handleDrawCards(event)}>Hit me</button>
     </>
   )
 };
