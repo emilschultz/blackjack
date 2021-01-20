5ac6ee2de0c6ffddf3a05464a0f6acf83084e168
Emil Schultz <emilrschultz@gmail.com>
2021-01-20 14:02:26 +0100

    shuffleCards functionality added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [cardDeck, setCardDeck] = useState('');
+  // const [dealerHand, setDealerHand] = useState('');
+  // const [playerHand, setPlayerHand] = useState('');
+  const [responseData, setResponseData] = useState('');
+
+  const handeClick = (event) => {
+    event.preventDefault();
+
+    let shuffleCards = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1' 
+      fetch(shuffleCards)
+        .then(response => {
+          console.log(response.status)
+          console.log(response.text)
+          return response.json();
+        })
+        .then(data => {
+          console.log(data)
+          setResponseData(data)
+        })
+  }
+
+
   return (
     <>
       <h1>Let's play blackjack!</h1>
+      <button type="submit" onClick={event => handeClick(event)}>Hit me</button>
     </>
   )
 };
